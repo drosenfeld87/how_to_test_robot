48274ea8c9f4237f41cae315abe18ace737011c4
debbie <drosenfeld87@gmail.com>
2018-03-09 15:46:28 -0500

    complete robot test assignment

--- test_robot.rb
+++ test_robot.rb
@@ -5,75 +5,84 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
     # arrange
-
+    r = Robot.new()
     # act
-
+    r.needs_repairs = true
+    r.foreign_model = true
+    expected_result = 1
     # assert
+    assert_equal(expected_result, r.station)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
-
+    r = Robot.new()
     # act
-
+    r.needs_repairs = true
+    r.vintage_model = true
+    expected_result = 2
     # assert
+    assert_equal(expected_result, r.station)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
-
+    r = Robot.new()
     # act
-
+    r.needs_repairs = true
+    expected_result = 3
     # assert
+    assert_equal(expected_result, r.station)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
-
+    r = Robot.new()
     # act
-
+    expected_result = 4
     # assert
+    assert_equal(expected_result, r.station)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
-
+    r = Robot.new()
     # act
-
+    r.todos.empty? == true
+    expected_result = -1
     # assert
+    assert_equal(expected_result, r.prioritize_tasks)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
-
+    r = Robot.new
+    r.todos=([1,2,3])
     # act
-
+    expected_result = 3
     # assert
+    assert_equal(expected_result, r.prioritize_tasks)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
     # arrange
-
+    r = Robot.new
+    r.day_off = ("saturday")
     # act
-
+    expected_result = false
     # assert
+    assert_equal(expected_result, r.workday?("saturday"))
   end
 
   def test_workday_not_day_off_returns_true
-    skip
     # arrange
-
+    r = Robot.new
+    r.day_off = (Time.now)
     # act
-
+    expected_result = true
     # assert
+    assert_equal(expected_result, r.workday?(Time.now))
   end
 
 end
